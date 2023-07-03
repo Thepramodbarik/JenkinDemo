099b5248fc76a3da02842c9dea6d0304169cfe76
Pramod barik <pramod.barik@cloverinfotech.com>
2023-07-03 10:53:56 +0530

    testttt

--- src/main/java/com/example/JenkinDemoApplication.java
+++ src/main/java/com/example/JenkinDemoApplication.java
@@ -9,7 +9,7 @@ public class JenkinDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinDemoApplication.class, args);
 
-		System.out.println("jenkin deployment test==111");
+		System.out.println("jenkin deployment test==1112");
 	}
 
 }
