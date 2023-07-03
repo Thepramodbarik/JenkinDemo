f7b5ea684ad9929ae8f3ee30ee373f6df6e6b444
Pramod barik <pramod.barik@cloverinfotech.com>
2023-07-03 11:18:23 +0530

    adding packaging tag under project tag

--- src/main/java/com/example/JenkinDemoApplication.java
+++ src/main/java/com/example/JenkinDemoApplication.java
@@ -9,7 +9,7 @@ public class JenkinDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinDemoApplication.class, args);
 
-		System.out.println("jenkin deployment test==1112");
+		System.out.println("jenkin deployment test==1113");
 	}
 
 }
